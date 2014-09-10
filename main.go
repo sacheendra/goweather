26edd93fcefb459d423d939b28b867c528dcb1eb
Sacheendra Talluri <sacheendra.t@gmail.com>
2014-09-10 17:44:38 +0530

    changes to xml parser from json

--- main.go
+++ main.go
@@ -4,34 +4,28 @@ import (
 //    "fmt"
     "net/http"
     "io/ioutil"
-    "encoding/json"
+    "encoding/xml"
     "html/template"
-    "errors"
 
     "appengine"
     "appengine/urlfetch"
 )
 
-type WeatherData struct {
-    TempMaxC string
-    TempMinC string
-}
-
-type LocationData struct {
-    Query string
-}
-
-type ResponseData struct {
-    Location string
-    Weather WeatherData
-}
-
 type Weather struct {
-    Data struct {
-        Weather []WeatherData
-        Request []LocationData
-        Error []map[string]string
-    }
+        XMLName string `xml:"data"`
+        Weather struct {
+            XMLName string `xml:"weather"`
+            TempMaxC string `xml:"tempMaxC"`
+            TempMinC string `xml:"tempMinC"`
+        }
+        Location struct {
+            XMLName string `xml:"request"`
+            Current string `xml:"query"`
+        }
+        Error struct {
+            XMLName string `xml:"error"`
+            Msg string `xml:"msg"`
+        }
 }
 
 func init() {
@@ -45,11 +39,11 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
         t, _ := template.ParseFiles("templates/input.tmpl")
         t.Execute(w, nil)
     } else if r.Method == "POST" {
-        getWeatherHandler(w, r)
+        getWeather(w, r)
     }
 }
 
-func getWeatherHandler(w http.ResponseWriter, r *http.Request) {
+func getWeather(w http.ResponseWriter, r *http.Request) {
     appengineContext := appengine.NewContext(r)
     client := urlfetch.Client(appengineContext)
 
@@ -59,36 +53,39 @@ func getWeatherHandler(w http.ResponseWriter, r *http.Request) {
         r.Form["location2"][0], 
         r.Form["location3"][0], 
         r.Form["location4"][0],
+        r.Form["location5"][0],
     }
 
-    weatherc := make(chan ResponseData)
+    weatherc := make(chan Weather)
     errc := make(chan error)
 
-    for i:=0; i<4; i++ {
+    for i:=0; i<5; i++ {
         go func (location string) {
             if location == "" {
-                weatherc <- ResponseData{Location: "", Weather: WeatherData{}}
+                weatherc <- Weather{}
             } else {
-                responseData, err := fetchResponseData(client, location)
+                weather, err := fetchWeather(client, location)
                 if err != nil {
                     errc <- err
                     return
                 } else {
-                    weatherc <- responseData
+                    weatherc <- weather
                 }
             }
         }(location[i])
     }
 
-    locationWeather := make([]ResponseData, 4)
+    locationWeather := make([]Weather, 5)
 
-    for i:=0; i<4; i++ {
+    for i:=0; i<5; i++ {
         select {
             case weather := <- weatherc:
                 locationWeather[i] = weather
             case err := <- errc:
                 if err != nil {
-                    http.Error(w, err.Error(), http.StatusInternalServerError)
+                    t, _ := template.ParseFiles("templates/error.tmpl")
+                    t.Execute(w, nil)
+                    //http.Error(w, err.Error(), http.StatusInternalServerError)
                     return
                 }
         }
@@ -96,7 +93,8 @@ func getWeatherHandler(w http.ResponseWriter, r *http.Request) {
 
     t, err := template.ParseFiles("templates/weather.tmpl")
     if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
+        t, _ := template.ParseFiles("templates/error.tmpl")
+        t.Execute(w, nil)
         return
     }
 
@@ -104,29 +102,23 @@ func getWeatherHandler(w http.ResponseWriter, r *http.Request) {
 //    fmt.Fprintf(w, "%s %s %s %s\n", locationWeather[0].Location, locationWeather[1].Location, locationWeather[2].Location, locationWeather[3].Location)
 }
 
-func fetchResponseData(client *http.Client, location string) (ResponseData, error) {
-    resp, err := client.Get("http://api.worldweatheronline.com/free/v1/weather.ashx?key=b07b0d3aafb4b1229d8d297c1c64d61637f6c5f4&q=" + location + "&format=json&date=today")
+func fetchWeather(client *http.Client, location string) (Weather, error) {
+    resp, err := client.Get("http://api.worldweatheronline.com/free/v1/weather.ashx?key=b07b0d3aafb4b1229d8d297c1c64d61637f6c5f4&q=" + location + "&format=xml&date=today")
     if err != nil {
-        return ResponseData{}, err
+        return Weather{}, err
     }
 
     contents, err := ioutil.ReadAll(resp.Body)
     resp.Body.Close()
     if err != nil {
-        return ResponseData{}, err
+        return Weather{}, err
     }
 
     var weather Weather
-    err = json.Unmarshal(contents, &weather)
+    err = xml.Unmarshal(contents, &weather)
     if err != nil {
-        return ResponseData{}, err
+        return Weather{}, err
     }
 
-    if len(weather.Data.Error) > 0 {
-        return ResponseData{}, errors.New(weather.Data.Error[0]["msg"])
-    }
-
-    responseData := ResponseData{Location: weather.Data.Request[0].Query, Weather: weather.Data.Weather[0]}
-
-    return responseData, nil
+    return weather, nil
 }
